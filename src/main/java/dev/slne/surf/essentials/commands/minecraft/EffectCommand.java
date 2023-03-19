b169932b6ba384330b49eb215e27b64e4e18d97b
twisti <twisti@slne.dev>
2023-03-19 10:41:09 +0100

    added infinitive option

--- src/main/java/dev/slne/surf/essentials/commands/minecraft/EffectCommand.java
+++ src/main/java/dev/slne/surf/essentials/commands/minecraft/EffectCommand.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableList;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import dev.slne.surf.essentials.SurfEssentials;
@@ -13,7 +14,6 @@ import dev.slne.surf.essentials.utils.color.EffectColors;
 import dev.slne.surf.essentials.utils.permission.Permissions;
 import io.papermc.paper.adventure.PaperAdventure;
 import net.kyori.adventure.text.event.HoverEvent;
-import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
@@ -21,7 +21,6 @@ import net.minecraft.commands.arguments.ResourceArgument;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
@@ -40,35 +39,27 @@ public class EffectCommand {
     private static void literal(LiteralArgumentBuilder<CommandSourceStack> literal){
         literal.requires(sourceStack -> sourceStack.hasPermission(2, Permissions.EFFECT_PERMISSION));
 
-        // Get the command build context for the server
-        CommandBuildContext buildContext = CommandBuildContext.configurable(MinecraftServer.getServer().registryAccess(),
-                MinecraftServer.getServer().getWorldData().getDataConfiguration().enabledFeatures());
-
         literal.then(Commands.literal("give")
                 .then(Commands.argument("targets", EntityArgument.entities())
-                        .then(Commands.argument("effect", ResourceArgument.resource(buildContext, Registries.MOB_EFFECT))
+                        .then(Commands.argument("effect", ResourceArgument.resource(EssentialsUtil.buildContext(), Registries.MOB_EFFECT))
                                 .executes(context -> giveEffect(context.getSource(), EntityArgument.getEntities(context, "targets"),
                                         ResourceArgument.getMobEffect(context, "effect"), null, 0, true))
 
                                 .then(Commands.argument("duration", IntegerArgumentType.integer(1, 1000000))
                                         .executes(context -> giveEffect(context.getSource(), EntityArgument.getEntities(context, "targets"),
                                                 ResourceArgument.getMobEffect(context, "effect"), IntegerArgumentType.getInteger(context, "duration"), 0, true))
+                                        .then(amplifierAndParticleBuilder(false)))
 
-                                        .then(Commands.argument("amplifier", IntegerArgumentType.integer(0, 255))
-                                                .executes(context -> giveEffect(context.getSource(), EntityArgument.getEntities(context, "targets"),
-                                                        ResourceArgument.getMobEffect(context, "effect"), IntegerArgumentType.getInteger(context, "duration"),
-                                                        IntegerArgumentType.getInteger(context, "amplifier"), true))
-
-                                                .then(Commands.argument("hideParticles", BoolArgumentType.bool())
-                                                        .executes(context -> giveEffect(context.getSource(), EntityArgument.getEntities(context, "targets"),
-                                                                ResourceArgument.getMobEffect(context, "effect"), IntegerArgumentType.getInteger(context, "duration"),
-                                                                IntegerArgumentType.getInteger(context, "amplifier"), !BoolArgumentType.getBool(context, "hideParticles")))))))));
+                                .then(Commands.literal("infinitive")
+                                        .executes(context -> giveEffect(context.getSource(), EntityArgument.getEntities(context, "targets"),
+                                                ResourceArgument.getMobEffect(context, "effect"), -1, 0, true))
+                                        .then(amplifierAndParticleBuilder(true))))));
 
         literal.then(Commands.literal("clear")
                 .executes(context -> clearAllEffects(context.getSource(), ImmutableList.of(context.getSource().getEntityOrException())))
                 .then(Commands.argument("targets", EntityArgument.entities())
                         .executes(context -> clearAllEffects(context.getSource(), EntityArgument.getEntities(context, "targets")))
-                        .then(Commands.argument("effect", ResourceArgument.resource(buildContext, Registries.MOB_EFFECT))
+                        .then(Commands.argument("effect", ResourceArgument.resource(EssentialsUtil.buildContext(), Registries.MOB_EFFECT))
                                 .executes(context -> clearSingleEffect(context.getSource(), EntityArgument.getEntities(context, "targets"), ResourceArgument.getMobEffect(context, "effect"))))));
     }
 
@@ -79,9 +70,9 @@ public class EffectCommand {
         int durationInTicks = calculateDurationInTicks(mobEffectList, seconds);
 
         for (Entity entity : targets) {
-            if (entity instanceof LivingEntity) {
-                MobEffectInstance mobeffect = new MobEffectInstance(mobEffectList, durationInTicks, amplifier, false, showParticles);
-                if (((LivingEntity) entity).addEffect(mobeffect, source.getEntity(), EntityPotionEffectEvent.Cause.COMMAND)) {
+            if (entity instanceof LivingEntity livingEntity) {
+                MobEffectInstance mobEffect = new MobEffectInstance(mobEffectList, durationInTicks, amplifier, false, showParticles);
+                if (livingEntity.addEffect(mobEffect, source.getEntity(), EntityPotionEffectEvent.Cause.COMMAND)) {
                     ++successfulApplies;
                 }
             }
@@ -208,17 +199,33 @@ public class EffectCommand {
     }
 
     private static int calculateDurationInTicks(MobEffect mobEffect, @Nullable Integer durationSeconds) {
+        int ticks;
         if (durationSeconds != null) {
             if (mobEffect.isInstantenous()) {
-                return durationSeconds;
+                ticks = durationSeconds;
+            } else if (durationSeconds == -1) {
+                ticks = -1;
             } else {
-                return durationSeconds * 20;
+                ticks = durationSeconds * 20;
             }
         } else if (mobEffect.isInstantenous()) {
-            return 1;
+            ticks = 1;
         } else {
-            return 600;
+            ticks = 600;
         }
+        return ticks;
+    }
+
+    private static RequiredArgumentBuilder<CommandSourceStack, Integer> amplifierAndParticleBuilder(boolean isInfinitive){
+        return Commands.argument("amplifier", IntegerArgumentType.integer(0, 255))
+                .executes(context -> giveEffect(context.getSource(), EntityArgument.getEntities(context, "targets"),
+                        ResourceArgument.getMobEffect(context, "effect"), isInfinitive ? -1 : IntegerArgumentType.getInteger(context, "duration"),
+                        IntegerArgumentType.getInteger(context, "amplifier"), true))
+
+                .then(Commands.argument("hideParticles", BoolArgumentType.bool())
+                        .executes(context -> giveEffect(context.getSource(), EntityArgument.getEntities(context, "targets"),
+                                ResourceArgument.getMobEffect(context, "effect"), isInfinitive ? -1 : IntegerArgumentType.getInteger(context, "duration"),
+                                IntegerArgumentType.getInteger(context, "amplifier"), !BoolArgumentType.getBool(context, "hideParticles"))));
     }
 
     private static final SimpleCommandExceptionType ERROR_GIVE_FAILED = new SimpleCommandExceptionType(Component.translatable("commands.effect.give.failed"));
