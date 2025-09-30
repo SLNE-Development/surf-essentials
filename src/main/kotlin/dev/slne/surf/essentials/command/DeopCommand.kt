d4672550a9e1d52dacc74e7a24215bad9be65313
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-30 19:19:08 +0200

    refactor: simplify `opCommand` and `deopCommand` by switching to single player selection

--- src/main/kotlin/dev/slne/surf/essentials/command/DeopCommand.kt
+++ src/main/kotlin/dev/slne/surf/essentials/command/DeopCommand.kt
@@ -2,59 +2,32 @@ package dev.slne.surf.essentials.command
 
 import dev.jorel.commandapi.kotlindsl.anyExecutor
 import dev.jorel.commandapi.kotlindsl.commandTree
-import dev.jorel.commandapi.kotlindsl.entitySelectorArgumentManyPlayers
+import dev.jorel.commandapi.kotlindsl.entitySelectorArgumentOnePlayer
 import dev.jorel.commandapi.kotlindsl.getValue
 import dev.slne.surf.essentials.util.EssentialsPermissionRegistry
-import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
 import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
-import dev.slne.surf.surfapi.core.api.util.mutableObjectSetOf
 import org.bukkit.entity.Player
 
 fun deopCommand() = commandTree("deop") {
     withPermission(EssentialsPermissionRegistry.DEOP_COMMAND)
-    entitySelectorArgumentManyPlayers("players") {
+    entitySelectorArgumentOnePlayer("player") {
         anyExecutor { executor, args ->
-            val players: Collection<Player> by args
-            val successfulPlayers = mutableObjectSetOf<Player>()
+            val player: Player by args
 
-            players.forEach {
-                if (it.isOp) {
-                    it.isOp = false
-                    successfulPlayers.add(it)
-                }
-            }
-
-            if (successfulPlayers.isEmpty()) {
+            if (!player.isOp) {
                 executor.sendText {
                     appendPrefix()
-                    error("Es wurde keinem Spieler der Operator-Status entzogen.")
+                    error("Der Spieler ist kein Operator.")
                 }
                 return@anyExecutor
             }
 
-            if (successfulPlayers.size < players.size) {
-                executor.sendText {
-                    appendPrefix()
-                    error("Einigen Spielern konnte der Operator-Status nicht entzogen werden, da sie keinen Operator-Status besitzen.")
-                    hoverEvent(buildText {
-                        info("Erfolgreich degradierte Spieler")
-                        info(":")
-                        appendSpace()
-                        variableValue(successfulPlayers.joinToString(", ") { it.name })
-                    })
-                }
-                return@anyExecutor
-            }
+            player.isOp = true
 
             executor.sendText {
                 appendPrefix()
-                success("Den ausgewählten Spielern wurde erfolgreich der Operator-Status entzogen.")
-                hoverEvent(buildText {
-                    info("Erfolgreich degradierte Spieler")
-                    info(":")
-                    appendSpace()
-                    variableValue(successfulPlayers.joinToString(", ") { it.name })
-                })
+                variableValue(player.name)
+                success(" ist nun ein Operator.")
             }
         }
     }
--- src/main/kotlin/dev/slne/surf/essentials/command/OpCommand.kt
+++ src/main/kotlin/dev/slne/surf/essentials/command/OpCommand.kt
@@ -2,59 +2,32 @@ package dev.slne.surf.essentials.command
 
 import dev.jorel.commandapi.kotlindsl.anyExecutor
 import dev.jorel.commandapi.kotlindsl.commandTree
-import dev.jorel.commandapi.kotlindsl.entitySelectorArgumentManyPlayers
+import dev.jorel.commandapi.kotlindsl.entitySelectorArgumentOnePlayer
 import dev.jorel.commandapi.kotlindsl.getValue
 import dev.slne.surf.essentials.util.EssentialsPermissionRegistry
-import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
 import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
-import dev.slne.surf.surfapi.core.api.util.mutableObjectSetOf
 import org.bukkit.entity.Player
 
 fun opCommand() = commandTree("op") {
     withPermission(EssentialsPermissionRegistry.OP_COMMAND)
-    entitySelectorArgumentManyPlayers("players") {
+    entitySelectorArgumentOnePlayer("player") {
         anyExecutor { executor, args ->
-            val players: Collection<Player> by args
-            val successfulPlayers = mutableObjectSetOf<Player>()
+            val player: Player by args
 
-            players.forEach {
-                if (!it.isOp) {
-                    it.isOp = true
-                    successfulPlayers.add(it)
-                }
-            }
-
-            if (successfulPlayers.isEmpty()) {
+            if (player.isOp) {
                 executor.sendText {
                     appendPrefix()
-                    error("Es wurde kein Spieler zum Operator ernannt.")
+                    error("Der Spieler ist bereits ein Operator.")
                 }
                 return@anyExecutor
             }
 
-            if (successfulPlayers.size < players.size) {
-                executor.sendText {
-                    appendPrefix()
-                    error("Einige Spieler konnten nicht zum Operator ernannt werden, da sie bereits Operator sind.")
-                    hoverEvent(buildText {
-                        info("Erfolgreich zum Operator ernannte Spieler")
-                        info(":")
-                        appendSpace()
-                        variableValue(successfulPlayers.joinToString(", ") { it.name })
-                    })
-                }
-                return@anyExecutor
-            }
+            player.isOp = true
 
             executor.sendText {
                 appendPrefix()
-                success("Die ausgewählten Spieler wurden erfolgreich zum Operator ernannt.")
-                hoverEvent(buildText {
-                    info("Erfolgreich zum Operator ernannte Spieler")
-                    info(":")
-                    appendSpace()
-                    variableValue(successfulPlayers.joinToString(", ") { it.name })
-                })
+                variableValue(player.name)
+                success(" ist nun ein Operator.")
             }
         }
     }
