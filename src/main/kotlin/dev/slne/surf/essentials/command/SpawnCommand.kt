e3ab17b89a303ab4a9aa9483c8ca216b501bd4f8
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-30 20:34:25 +0200

    feat: add `spawnCommand` to teleport players to spawn with optional world argument and feedback messages

---
+++ src/main/kotlin/dev/slne/surf/essentials/command/SpawnCommand.kt
@@ -0,0 +1,51 @@
+package dev.slne.surf.essentials.command
+
+import dev.jorel.commandapi.kotlindsl.commandTree
+import dev.jorel.commandapi.kotlindsl.getValue
+import dev.jorel.commandapi.kotlindsl.playerExecutor
+import dev.jorel.commandapi.kotlindsl.worldArgument
+import dev.slne.surf.essentials.util.EssentialsPermissionRegistry
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import org.bukkit.World
+import org.bukkit.event.player.PlayerTeleportEvent
+
+fun spawnCommand() = commandTree("spawn") {
+    withPermission(EssentialsPermissionRegistry.SPAWN_COMMAND)
+    playerExecutor { player, _ ->
+        player.sendText {
+            appendPrefix()
+            info("Du wirst zum Spawn teleportiert...")
+        }
+
+        player.teleportAsync(player.world.spawnLocation, PlayerTeleportEvent.TeleportCause.COMMAND)
+            .thenRun {
+                player.sendText {
+                    appendPrefix()
+                    success("Du wurdest zum Spawn teleportiert.")
+                }
+            }
+    }
+
+    worldArgument("world") {
+        playerExecutor { player, args ->
+            val world: World by args
+
+            player.sendText {
+                appendPrefix()
+                info("Du wirst zum Spawn der Welt ")
+                variableValue(world.name)
+                info(" teleportiert...")
+            }
+
+            player.teleportAsync(world.spawnLocation, PlayerTeleportEvent.TeleportCause.COMMAND)
+                .thenRun {
+                    player.sendText {
+                        appendPrefix()
+                        success("Du wurdest zum Spawn der Welt ")
+                        variableValue(world.name)
+                        success(" teleportiert.")
+                    }
+                }
+        }
+    }
+}
