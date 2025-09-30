ecfad434d973a02cba481c4e3326c2dfff2dec66
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-30 16:35:15 +0200

    feat: add `EssentialsPermissionRegistry` for managing permissions within Surf Essentials

---
+++ src/main/kotlin/dev/slne/surf/essentials/util/EssentialsPermissionRegistry.kt
@@ -0,0 +1,11 @@
+package dev.slne.surf.essentials.util
+
+import dev.slne.surf.surfapi.bukkit.api.permission.PermissionRegistry
+
+object EssentialsPermissionRegistry : PermissionRegistry() {
+    const val PREFIX = "surf.essentials"
+
+    val FLY_COMMAND = create("$PREFIX.fly.command")
+    val GAME_MODE_COMMAND = create("$PREFIX.gameMode.command")
+    val GAME_MODE_SWITCHER = create("$PREFIX.gameMode.switcher")
+}
