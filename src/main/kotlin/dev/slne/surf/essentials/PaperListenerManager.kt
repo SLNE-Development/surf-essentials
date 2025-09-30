d5cf4c2b31e59fe7ea14e7798c2bfa93e9005160
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-30 16:34:56 +0200

    chore: remove `DeathListener` and update `PaperListenerManager` registrations

--- src/main/kotlin/dev/slne/surf/essentials/PaperListenerManager.kt
+++ src/main/kotlin/dev/slne/surf/essentials/PaperListenerManager.kt
@@ -1,7 +1,14 @@
 package dev.slne.surf.essentials
 
+import dev.slne.surf.essentials.listener.*
+import dev.slne.surf.surfapi.bukkit.api.event.register
+
 object PaperListenerManager {
     fun registerAll() {
-
+        AdvancementListener.register()
+        FlyCorrectionListener.register()
+        GameModeSwitcherCorrectionListener.register()
+        TeleportationListener.register()
+        UnknownCommandListener.register()
     }
 }
