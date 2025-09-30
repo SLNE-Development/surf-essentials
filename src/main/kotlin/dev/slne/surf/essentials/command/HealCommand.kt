a1df40a7edad5ee2b11ef4c2dde4ff6a8864b2e2
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-30 19:53:35 +0200

    feat: add `healCommand` to restore player health with permission check

---
+++ src/main/kotlin/dev/slne/surf/essentials/command/HealCommand.kt
@@ -0,0 +1,37 @@
+package dev.slne.surf.essentials.command
+
+import dev.jorel.commandapi.kotlindsl.*
+import dev.slne.surf.essentials.util.EssentialsPermissionRegistry
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import org.bukkit.attribute.Attribute
+import org.bukkit.entity.Player
+
+fun healCommand() = commandTree("heal") {
+    withPermission(EssentialsPermissionRegistry.HEAL_COMMAND)
+    playerExecutor { player, _ ->
+        player.health = player.getAttribute(Attribute.MAX_HEALTH)?.value ?: 20.0
+        player.sendText {
+            appendPrefix()
+            success("Du hast dich geheilt.")
+        }
+    }
+    
+    entitySelectorArgumentOnePlayer("player") {
+        anyExecutor { executor, args ->
+            val player: Player by args
+
+            player.health = player.getAttribute(Attribute.MAX_HEALTH)?.value ?: 20.0
+
+            executor.sendText {
+                appendPrefix()
+                variableValue(player.name)
+                success(" wurde geheilt.")
+            }
+
+            player.sendText {
+                appendPrefix()
+                success("Du wurdest geheilt.")
+            }
+        }
+    }
+}
