3ccf6f93fe60e9659b07d7088759a80bcb2d7c79
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-30 19:24:22 +0200

    feat: add `flyCommand` to toggle player flight mode

--- src/main/kotlin/dev/slne/surf/essentials/PaperCommandManager.kt
+++ src/main/kotlin/dev/slne/surf/essentials/PaperCommandManager.kt
@@ -3,6 +3,7 @@ package dev.slne.surf.essentials
 import dev.jorel.commandapi.CommandAPI
 import dev.slne.surf.essentials.command.advancementCommand
 import dev.slne.surf.essentials.command.deopCommand
+import dev.slne.surf.essentials.command.flyCommand
 import dev.slne.surf.essentials.command.opCommand
 
 object PaperCommandManager {
@@ -14,5 +15,6 @@ object PaperCommandManager {
         advancementCommand()
         opCommand()
         deopCommand()
+        flyCommand()
     }
 }
---
+++ src/main/kotlin/dev/slne/surf/essentials/command/FlyCommand.kt
@@ -0,0 +1,66 @@
+package dev.slne.surf.essentials.command
+
+import dev.jorel.commandapi.kotlindsl.*
+import dev.slne.surf.essentials.util.EssentialsPermissionRegistry
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import org.bukkit.entity.Player
+
+fun flyCommand() = commandTree("fly") {
+    withPermission(EssentialsPermissionRegistry.FLY_COMMAND)
+    playerExecutor { player, args ->
+        if (player.allowFlight) {
+            player.allowFlight = false
+            player.isFlying = false
+
+
+            player.sendText {
+                appendPrefix()
+                success("Du kannst nun nicht mehr fliegen.")
+            }
+        } else {
+            player.allowFlight = true
+            player.isFlying = true
+
+            player.sendText {
+                appendPrefix()
+                success("Du kannst nun fliegen.")
+            }
+        }
+    }
+    entitySelectorArgumentOnePlayer("player") {
+        anyExecutor { executor, args ->
+            val player: Player by args
+
+            if (player.allowFlight) {
+                player.allowFlight = false
+                player.isFlying = false
+
+
+                executor.sendText {
+                    appendPrefix()
+                    variableValue(player.name)
+                    success(" kann nun nicht mehr fliegen.")
+                }
+
+                player.sendText {
+                    appendPrefix()
+                    success("Du kannst nun nicht mehr fliegen.")
+                }
+            } else {
+                player.allowFlight = true
+                player.isFlying = true
+
+                executor.sendText {
+                    appendPrefix()
+                    variableValue(player.name)
+                    success(" kann nun fliegen.")
+                }
+
+                player.sendText {
+                    appendPrefix()
+                    success("Du kannst nun fliegen.")
+                }
+            }
+        }
+    }
+}
