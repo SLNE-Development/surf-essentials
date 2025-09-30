6f24f33c9d93ffd9b742920dbd1be64eec708d6d
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-30 19:04:17 +0200

    refactor: unregister conflicting commands before re-registering replacements

--- src/main/kotlin/dev/slne/surf/essentials/PaperCommandManager.kt
+++ src/main/kotlin/dev/slne/surf/essentials/PaperCommandManager.kt
@@ -1,9 +1,18 @@
 package dev.slne.surf.essentials
 
+import dev.jorel.commandapi.CommandAPI
 import dev.slne.surf.essentials.command.advancementCommand
+import dev.slne.surf.essentials.command.deopCommand
+import dev.slne.surf.essentials.command.opCommand
 
 object PaperCommandManager {
     fun registerAll() {
+        CommandAPI.unregister("op")
+        CommandAPI.unregister("deop")
+        CommandAPI.unregister("advancement")
+
         advancementCommand()
+        opCommand()
+        deopCommand()
     }
 }
