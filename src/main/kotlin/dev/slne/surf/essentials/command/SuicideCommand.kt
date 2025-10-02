04936ef18a2108e89c56cb68529f7f505ccf2b31
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-10-03 01:02:47 +0200

    feat: add `SuicideCommand` to allow players to self-destruct or target other players

---
+++ src/main/kotlin/dev/slne/surf/essentials/command/SuicideCommand.kt
@@ -0,0 +1,36 @@
+package dev.slne.surf.essentials.command
+
+import dev.jorel.commandapi.kotlindsl.*
+import dev.slne.surf.essentials.util.EssentialsPermissionRegistry
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import org.bukkit.entity.Player
+
+fun suicideCommand() = commandTree("suicide") {
+    withPermission(EssentialsPermissionRegistry.SUICIDE_COMMAND)
+    playerExecutor { player, _ ->
+        player.inventory.clear()
+        player.health = 0.0
+        player.sendHealthUpdate()
+
+        player.sendText {
+            appendPrefix()
+            success("Du wurdest auf tragische Weise getötet!")
+        }
+    }
+
+    entitySelectorArgumentOnePlayer("player") {
+        withPermission(EssentialsPermissionRegistry.SUICIDE_COMMAND_OTHER)
+        anyExecutor { executor, args ->
+            val player: Player by args
+
+            player.inventory.clear()
+            player.health = 0.0
+            player.sendHealthUpdate()
+
+            player.sendText {
+                appendPrefix()
+                success("${executor.name} starb auf tragischer Weise.")
+            }
+        }
+    }
+}
