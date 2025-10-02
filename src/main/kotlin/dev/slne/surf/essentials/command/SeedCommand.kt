d80f551f6dc72bc8aa003e201fea510edfb5789b
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-10-03 01:02:15 +0200

    feat: add `SeedCommand` to display world seed with clipboard copy functionality

---
+++ src/main/kotlin/dev/slne/surf/essentials/command/SeedCommand.kt
@@ -0,0 +1,46 @@
+package dev.slne.surf.essentials.command
+
+import dev.jorel.commandapi.kotlindsl.*
+import dev.slne.surf.essentials.util.EssentialsPermissionRegistry
+import dev.slne.surf.surfapi.core.api.font.toSmallCaps
+import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
+import dev.slne.surf.surfapi.core.api.messages.adventure.clickCopiesToClipboard
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import org.bukkit.World
+
+fun seedCommand() = commandTree("seed") {
+    withPermission(EssentialsPermissionRegistry.SEED_COMMAND)
+    playerExecutor { player, _ ->
+        val world = player.world
+        player.sendText {
+            appendPrefix()
+            info("Der Ursprungswert der Welt ")
+            variableValue(world.name)
+            info(" ist ")
+            variableValue(world.seed)
+            info(".")
+            clickCopiesToClipboard(world.seed.toString())
+            hoverEvent(buildText {
+                info("Klicke, um den Seed zu kopieren.".toSmallCaps())
+            })
+        }
+    }
+    worldArgument("world") {
+        anyExecutor { executor, args ->
+            val world: World by args
+
+            executor.sendText {
+                appendPrefix()
+                info("Der Ursprungswert der Welt ")
+                variableValue(world.name)
+                info(" ist ")
+                variableValue(world.seed)
+                info(".")
+                clickCopiesToClipboard(world.seed.toString())
+                hoverEvent(buildText {
+                    info("Klicke, um den Seed zu kopieren.".toSmallCaps())
+                })
+            }
+        }
+    }
+}
