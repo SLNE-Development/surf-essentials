5a2c3cc50e692a2a4938faa3356510aa2f6207ee
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-10-03 01:02:21 +0200

    feat: add `SetPlayerIdleTimeoutCommand` to configure server idle timeout in minutes

---
+++ src/main/kotlin/dev/slne/surf/essentials/command/SetPlayerIdleTimeoutCommand.kt
@@ -0,0 +1,26 @@
+package dev.slne.surf.essentials.command
+
+import dev.jorel.commandapi.kotlindsl.anyExecutor
+import dev.jorel.commandapi.kotlindsl.commandTree
+import dev.jorel.commandapi.kotlindsl.getValue
+import dev.jorel.commandapi.kotlindsl.integerArgument
+import dev.slne.surf.essentials.util.EssentialsPermissionRegistry
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import org.bukkit.Bukkit
+
+fun setPlayerIdleTimeoutCommand() = commandTree("setidletimeout") {
+    withPermission(EssentialsPermissionRegistry.PLAYER_IDLE_TIMEOUT_COMMAND)
+    integerArgument("minutes") {
+        anyExecutor { executor, args ->
+            val minutes: Int by args
+
+            Bukkit.setIdleTimeout(minutes)
+            executor.sendText {
+                appendPrefix()
+                success("Die Inaktivitätszeit wurde auf ")
+                variableValue("$minutes Minuten")
+                success(" gesetzt.")
+            }
+        }
+    }
+}
