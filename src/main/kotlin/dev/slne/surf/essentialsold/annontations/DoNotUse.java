122250759269bd0717435cf119fe9edaffc31df3
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-10-16 16:18:59 +0200

    feat: remove old essentials

--- src/main/kotlin/dev/slne/surf/essentialsold/annontations/DoNotUse.java
+++
@@ -1,9 +0,0 @@
-package dev.slne.surf.essentialsold.annontations;
-
-import javax.annotation.meta.TypeQualifierDefault;
-import java.lang.annotation.ElementType;
-
-@TypeQualifierDefault({ElementType.METHOD, ElementType.PACKAGE, ElementType.MODULE, ElementType.TYPE})
-public @interface DoNotUse {
-    String value() default "";
-}
