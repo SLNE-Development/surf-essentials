4c831c50f2dbc18ec6d6027a6ce7732f6556f697
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-12-04 17:57:28 +0100

    feat: add amount argument to give command for item distribution

--- src/main/kotlin/dev/slne/surf/essentials/command/minecraft/GiveComamnd.kt
+++ src/main/kotlin/dev/slne/surf/essentials/command/minecraft/GiveComamnd.kt
@@ -38,6 +38,40 @@ fun giveCommand() = commandTree("give") {
                     }
                 }
             }
+
+            integerArgument("amount") {
+                anyExecutor { executor, args ->
+                    val amount: Int by args
+                    val itemStack: ItemStack by args
+                    val players: Collection<Player> by args
+
+                    val itemToGive = itemStack.clone().apply { this.amount = amount }
+
+                    players.forEach { it.inventory.addItem(itemToGive) }
+
+                    executor.sendText {
+                        appendPrefix()
+                        success("Du hast ")
+                        variableValue(amount.toString())
+                        success("x ")
+                        variableValue(itemStack.type.name)
+                        success(" an ")
+                        variableValue(players.size.toString())
+                        success(" Spieler vergeben.")
+                    }
+
+                    players.forEach { player ->
+                        player.sendText {
+                            appendPrefix()
+                            success("Du hast ")
+                            variableValue(amount.toString())
+                            success("x ")
+                            variableValue(itemStack.type.name)
+                            success(" erhalten.")
+                        }
+                    }
+                }
+            }
         }
     }
 }
