1288b39bbdb0c22fdc3fb7cee354c650270ec527
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-04-02 11:42:16 +0200

    fix: prevent entities from passing through locked portals

--- src/main/kotlin/dev/slne/surf/essentials/listener/CanvasListener.kt
+++ src/main/kotlin/dev/slne/surf/essentials/listener/CanvasListener.kt
@@ -17,26 +17,32 @@ import org.bukkit.event.player.PlayerTeleportEvent
 object CanvasListener : Listener {
     @EventHandler
     fun onPortal(event: EntityPortalAsyncEvent) {
-        val player = event.entity as? Player ?: return
         val world = event.to
 
-        if (worldService.isLocked(world)) {
-            if (!player.hasPermission(EssentialsPermissionRegistry.WORLD_BYPASS)) {
-                event.cancel()
-                player.sendText {
-                    appendErrorPrefix()
+        if (!worldService.isLocked(world)) {
+            return
+        }
 
-                    when (world.environment) {
-                        World.Environment.NETHER -> error("Der Nether ist zurzeit deaktiviert.")
-                        World.Environment.THE_END -> error("Das End ist zurzeit deaktiviert.")
-                        else -> error("Du kannst dieses Portal nicht benutzen!")
-                    }
+        val player = event.entity as? Player ?: run {
+            event.cancel()
+            return
+        }
+
+        if (!player.hasPermission(EssentialsPermissionRegistry.WORLD_BYPASS)) {
+            event.cancel()
+            player.sendText {
+                appendErrorPrefix()
+
+                when (world.environment) {
+                    World.Environment.NETHER -> error("Der Nether ist zurzeit deaktiviert.")
+                    World.Environment.THE_END -> error("Das End ist zurzeit deaktiviert.")
+                    else -> error("Du kannst dieses Portal nicht benutzen!")
                 }
-            } else {
-                player.sendText {
-                    appendSuccessPrefix()
-                    success("Du hast die Portal-Sperre umgangen.")
-                }
+            }
+        } else {
+            player.sendText {
+                appendSuccessPrefix()
+                success("Du hast die Portal-Sperre umgangen.")
             }
         }
     }
