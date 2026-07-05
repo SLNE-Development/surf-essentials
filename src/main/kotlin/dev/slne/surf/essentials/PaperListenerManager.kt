e356d9545ce6231027d3c5610fc1e40589ca7eba
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-07-05 19:59:11 +0200

    feat: implement spectator fly state management on game mode change

--- src/main/kotlin/dev/slne/surf/essentials/PaperListenerManager.kt
+++ src/main/kotlin/dev/slne/surf/essentials/PaperListenerManager.kt
@@ -14,5 +14,6 @@ object PaperListenerManager {
         SpecialItemListener.register()
         HungerListener.register()
         RestartListener.register()
+        SpectatorFlyListener.register()
     }
 }
---
+++ src/main/kotlin/dev/slne/surf/essentials/listener/SpectatorFlyListener.kt
@@ -0,0 +1,54 @@
+package dev.slne.surf.essentials.listener
+
+import dev.slne.surf.api.core.util.mutableObjectSetOf
+import dev.slne.surf.essentials.plugin
+import org.bukkit.GameMode
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerGameModeChangeEvent
+import org.bukkit.event.player.PlayerQuitEvent
+import java.util.*
+
+object SpectatorFlyListener : Listener {
+    private val allowFlightPlayers = mutableObjectSetOf<UUID>()
+    private val flyingPlayers = mutableObjectSetOf<UUID>()
+
+    @EventHandler
+    fun onGameModeChange(event: PlayerGameModeChangeEvent) {
+        val player = event.player
+        val previousGameMode = player.gameMode
+        val newGameMode = event.newGameMode
+
+        val isOldFlightMode =
+            previousGameMode == GameMode.SPECTATOR || previousGameMode == GameMode.CREATIVE
+        val isNewFlightMode = newGameMode == GameMode.SPECTATOR || newGameMode == GameMode.CREATIVE
+
+        if (!isOldFlightMode && isNewFlightMode) {
+            if (player.allowFlight) {
+                allowFlightPlayers.add(player.uniqueId)
+                if (player.isFlying) {
+                    flyingPlayers.add(player.uniqueId)
+                }
+            }
+            return
+        }
+
+        if (isOldFlightMode && !isNewFlightMode) {
+            val hadAllowFlight = allowFlightPlayers.remove(player.uniqueId)
+            val wasFlying = flyingPlayers.remove(player.uniqueId)
+
+            player.scheduler.runDelayed(plugin, {
+                if (player.isOnline) {
+                    player.allowFlight = hadAllowFlight
+                    player.isFlying = wasFlying && hadAllowFlight
+                }
+            }, null, 1L)
+        }
+    }
+
+    @EventHandler
+    fun onQuit(event: PlayerQuitEvent) {
+        allowFlightPlayers.remove(event.player.uniqueId)
+        flyingPlayers.remove(event.player.uniqueId)
+    }
+}
