c03274a0d538643fa44b5252ad82f0523f87d77f
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-07-08 12:07:36 +0200

    feat: enhance test sound command with pitch and volume arguments

--- src/main/kotlin/dev/slne/surf/essentials/command/TestSoundCommand.kt
+++ src/main/kotlin/dev/slne/surf/essentials/command/TestSoundCommand.kt
@@ -1,18 +1,49 @@
 package dev.slne.surf.essentials.command
 
-import dev.jorel.commandapi.arguments.FloatArgument
 import dev.jorel.commandapi.kotlindsl.*
 import dev.slne.surf.api.core.messages.adventure.playSound
 import dev.slne.surf.api.core.messages.adventure.sendText
 import dev.slne.surf.api.paper.util.BukkitSound
 import dev.slne.surf.essentials.util.permission.EssentialsPermissionRegistry
+import org.bukkit.Registry
 
 fun testSoundCommand() = commandTree("testsound") {
     withPermission(EssentialsPermissionRegistry.TEST_SOUND_COMMAND)
 
     soundArgument("sound") {
+        playerExecutor { player, arguments ->
+            val sound: BukkitSound by arguments
+
+            player.playSound(true) {
+                type(sound)
+            }
+
+            player.sendText {
+                appendSuccessPrefix()
+                success("Der Sound ")
+                variableValue(Registry.SOUNDS.getKey(sound)?.key.toString())
+                success(" wurde erfolgreich abgespielt.")
+            }
+        }
         floatArgument("pitch") {
-            optionalArgument(FloatArgument("volume")) {
+            playerExecutor { player, arguments ->
+                val sound: BukkitSound by arguments
+                val pitch: Float by arguments
+
+                player.playSound(true) {
+                    type(sound)
+                    pitch(pitch)
+                }
+
+                player.sendText {
+                    appendSuccessPrefix()
+                    success("Der Sound ")
+                    variableValue(Registry.SOUNDS.getKey(sound)?.key.toString())
+                    success(" wurde erfolgreich abgespielt.")
+                }
+            }
+
+            floatArgument("volume") {
                 playerExecutor { player, arguments ->
                     val sound: BukkitSound by arguments
                     val pitch: Float by arguments
@@ -29,7 +60,7 @@ fun testSoundCommand() = commandTree("testsound") {
                     player.sendText {
                         appendSuccessPrefix()
                         success("Der Sound ")
-                        variableValue(sound.toString())
+                        variableValue(Registry.SOUNDS.getKey(sound)?.key.toString())
                         success(" wurde erfolgreich abgespielt.")
                     }
                 }
